d5846658b4d7c1d6a3c8bf2fae104b69ebad0c70
Mwau stephen <stephenmwau41@gmail.com>
2022-03-10 15:14:40 +0300

    got the current position using the navigator Api method

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// 1.Get current position
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(lat, long);
+      console.log(position);
+    },
+    function () {
+      alert("Could not get current position");
+    }
+  );
