f2229f02f6c6e87d8b0c8ee8b64c33d993d95705
Mwau stephen <stephenmwau41@gmail.com>
2022-03-10 16:18:06 +0300

    added the leaflet functions to display the map

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ if (navigator.geolocation)
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       console.log(position);
+
+      //   2.After getting coords display map
+      var map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Could not get current position");
