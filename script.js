e434be4e9c6dc26d9296505024c14f36b63b598a
Mwau stephen <stephenmwau41@gmail.com>
2022-03-10 16:33:16 +0300

    added the coords array variable

--- script.js
+++ script.js
@@ -20,14 +20,18 @@ if (navigator.geolocation)
       console.log(position);
 
       //   2.After getting coords display map
-      var map = L.map("map").setView([51.505, -0.09], 13);
 
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      const coords = [latitude, longitude];
+      var map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
+          //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          //     attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
         .openPopup();
